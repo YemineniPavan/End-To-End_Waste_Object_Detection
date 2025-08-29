53dbe709de0c4bf68876c6d6466e0aeeb3bedb3c
YemineniPavan <yeminenipavan27@gmail.com>
2025-08-29 13:30:21 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Waste_Detection",
+    version= '0.0.0',
+    author= 'Yemineni Pavan',
+    author_email= "yeminenipavan27@gmail.com",
+    packages= find_packages(),
+    install_requires= []
+
+) 
